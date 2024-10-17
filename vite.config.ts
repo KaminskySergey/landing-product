783ecbc94ce8066c31b65dd01cf96ffc4f01c0c1
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2024-10-17 20:49:36 +0300

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_API_BOT_TOKEN': JSON.stringify(process.env.VITE_API_BOT_TOKEN),
+    'process.env.VITE_ID_CHAT': JSON.stringify(process.env.VITE_ID_CHAT)
+  }
 })
