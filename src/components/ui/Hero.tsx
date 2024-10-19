cfe7046025047bac298d4340ca1d019ccb99672e
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2024-10-19 15:05:22 +0300

    fix title

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -16,7 +16,7 @@ export function Hero({ scrollToForm }: IHero) {
             <div className="w-full md:w-1/2">
                 <div className="">
 
-                    <h1 className="mb-6 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Килимок з підігрівом</span> Folem KM Чорний від USB (Power Bank).</h1>
+                    <h1 className="mb-6 text-3xl font-extrabold text-gray-900  md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Килимок з підігрівом</span> Folem KM Чорний від USB (Power Bank).</h1>
 
                 </div>
                 <div>
